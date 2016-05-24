9e38576223c023a097b8c9434277f152e0c17182
Jonnatas Cabral <matiasjonnatas@gmail.com>
2016-05-24 15:32:38 -0300

    add dependencies

--- lend/apps/core/utils.py
+++ lend/apps/core/utils.py
@@ -1,14 +1,12 @@
 from io import BytesIO
 from docker import Client
-from core.constants import dockerfile
-from core.constants import base_url
 
-def create_image(dockerfile, base_url, tag='new_image',rm=True):
-	f = BytesIO(dockerfile.encode('utf-8'))
-	cli = Client(base_url=base_url)
 
-	response = [line for line in cli.build(
-	    fileobj=f, rm=rm, tag=tag
-	)]
-	return response
-	
+def create_image(dockerfile, base_url, tag='new_image', rm=True):
+    f = BytesIO(dockerfile.encode('utf-8'))
+    cli = Client(base_url=base_url)
+
+    response = [line for line in cli.build(
+        fileobj=f, rm=rm, tag=tag
+    )]
+    return response
