6da6513b898e1c961428ec2edfef16071122dd91
Victor Ferraz <victorfsf.dev@gmail.com>
2016-06-24 18:01:00 -0300

    Atualizando LogoutView

--- lend/apps/core/urls.py
+++ lend/apps/core/urls.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from allauth.account.views import logout
 from allauth.socialaccount.providers.github.views import oauth2_callback
 from allauth.socialaccount.providers.github.views import oauth2_login
 from core.views import index_view
 from core.views import login_view
+from core.views import logout_view
 from django.conf.urls import include
 from django.conf.urls import url
 
 accounts_patterns = [
+    url(r'^logout/$', logout_view, name='logout'),
     url(r'^login/$', oauth2_login, name='github_login'),
-    url(r'^logout/$', logout, name='logout'),
     url(r'^login/callback/$', oauth2_callback, name='github_callback'),
 ]
 
 urlpatterns = [
-    url(r'^$', index_view, name='index'),
     url(r'^accounts/', include(accounts_patterns)),
     url(r'^login/', login_view, name='login'),
+    url(r'^$', index_view, name='index'),
 ]
--- lend/apps/core/views/index.py
+++ lend/apps/core/views/index.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.views.generic import TemplateView
+from django.views.generic import RedirectView
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import logout
 
 
 class IndexView(TemplateView):
@@ -10,6 +14,22 @@ class IndexView(TemplateView):
 class LoginView(TemplateView):
     template_name = 'core/login.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('index'))
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+
+class LogoutView(RedirectView):
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
+
+    def get_redirect_url(self):
+        return reverse('index')
+
 
 index_view = IndexView.as_view()
 login_view = LoginView.as_view()
+logout_view = LogoutView.as_view()
